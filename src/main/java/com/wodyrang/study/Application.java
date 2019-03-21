656cef4ed3c17cba7d6731b3278d3679c58c3aca
정재요 <wody@genieworks.net>
2019-03-22 07:14:48 +0900

    commit Application class and pom.xml for Spring boot!

--- src/main/java/com/wodyrang/study/Application.java
+++ src/main/java/com/wodyrang/study/Application.java
@@ -1,12 +1,12 @@
 package com.wodyrang.study;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Spring boot Application Start!!
  */
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     /**
---
+++ src/test/java/com/wodyrang/study/ApplicationTest.java
@@ -0,0 +1,20 @@
+package com.wodyrang.study;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * TEST!.
+ * @author jaeyo
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void test() {
+
+    }
+}
